e174c328184b6acc1885dd78436d4d9ab255fd7c
dvallesp <davidvallesperez@gmail.com>
2015-07-09 23:24:45 +0200

    Añadida página de contacto con validación y protocolo POST. Modificado tamaño máximo sm para evitar bug iPad Mini Portrait-Mode

--- es/contacta.php
+++ es/contacta.php
@@ -37,7 +37,7 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul class="nav navbar-nav">
-                <li class="active"><a href="index.html">Inicio</a></li>
+                <li><a href="index.html">Inicio</a></li>
                 <li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">Proyectos <span class="caret"></span></a>
                   <ul class="dropdown-menu" role="menu">
@@ -65,62 +65,111 @@
               </ul>
               <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">Blog</a></li>
-                <li><a href="contacta.php">Contacta</a></li>
+                <li class="active"><a href="contacta.php">Contacta</a></li>
               </ul>
             </div><!-- /.navbar-collapse -->
           </div><!-- /.container-fluid -->
         </nav>
       </div><!-- /.header -->
 
-      <!-- /.carousel -->
-      <div class="row"> <!-- /.row del carousel -->
-        <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1"> <!-- /.thight only lg and md -->
-          <div id="carousel-main" class="carousel slide center" data-ride="carousel" style="margin-bottom: 20px">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-              <li data-target="#carousel-main" data-slide-to="0" class="active"></li>
-              <li data-target="#carousel-main" data-slide-to="1"></li>
-              <li data-target="#carousel-main" data-slide-to="2"></li>
-              <li data-target="#carousel-main" data-slide-to="3"></li>
-              <li data-target="#carousel-main" data-slide-to="4"></li>
-            </ol>
+      <ol class="breadcrumb">
+        <li><a href="./index.html">Inicio</a></li>
+        <li class="active">Contacta</li>
+      </ol>
 
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner " role="listbox">
-              <div class="item active">
-                <img src="../images/carousel-main/carousel1.jpeg" style="width: 100%" alt="Carousel 1" />
-                <div class="carousel-caption"></div>
+      <?php
+  if (isset($_POST["submit"])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $human = intval($_POST['human']);
+    $from = 'Bústia de missatges – Josepsanjuan.com'; 
+    $to = 'davidvallesperez@gmail.com'; 
+    $subject = 'Missatge a Josepsanjuan.com ';
+    
+    $body ="De: $name\n E-Mail: $email\n Missatge:\n $message";
+    // Check if name has been entered
+    if (!$_POST['name']) {
+      $errName = 'Por favor, introduce tu nombre.';
+    }
+    
+    // Check if email has been entered and is valid
+    if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+      $errEmail = 'Por favor, introduce una dirección e-mail válida';
+    }
+    
+    //Check if message has been entered
+    if (!$_POST['message']) {
+      $errMessage = 'Por favor, introduce un mensaje';
+    }
+    //Check if simple anti-bot test is correct
+    if ($human !== 5) {
+      $errHuman = 'No has superado el filtro anti-spam. Por favor, vuelve a intentarlo.';
+    }
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+  if (mail ($to, $subject, $body, $from)) {
+    $result='<div class="alert alert-success">¡Gracias! Te responderé lo antes posible.</div>';
+  } else {
+    $result='<div class="alert alert-danger">Disculpa, ha habido algún error al enviar tu mensaje. Por favor, vuelve a intentarlo</div>';
+  }
+}
+  }
+?>
+      <div class="container"> <!-- /.container del form -->
+        <h3>Escríbeme</h3>
+        <div class="row">
+          <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6" id="form"><!--formulario-->
+            <form class="form-horizontal" role="form" method="post" action="contacta.php">
+              <div class="form-group">
+                  <label for="name" class="col-sm-2 control-label">Nombre</label>
+                  <div class="col-sm-10">
+                      <input type="text" class="form-control" id="name" name="name" placeholder="Tu nombre y apellidos" value="<?php echo htmlspecialchars($_POST['name']); ?>">
+                      <?php echo "<p class='text-danger'>$errName</p>";?>
+                  </div>
               </div>
-              <div class="item">
-                <img src="../images/carousel-main/carousel2.jpeg" style="width: 100%" alt="Carousel 2" />
-                <div class="carousel-caption"></div>
+              <div class="form-group">
+                  <label for="email" class="col-sm-2 control-label">E-mail</label>
+                  <div class="col-sm-10">
+                      <input type="email" class="form-control" id="email" name="email" placeholder="ejemplo@dominio.com" value="<?php echo htmlspecialchars($_POST['email']); ?>">
+                      <?php echo "<p class='text-danger'>$errEmail</p>";?>
+                  </div>
               </div>
-              <div class="item">
-                <img src="../images/carousel-main/carousel3.jpeg" style="width: 100%" alt="Carousel 3" />
-                <div class="carousel-caption"></div>
+              <div class="form-group">
+                  <label for="message" class="col-sm-2 control-label">Mensaje</label>
+                  <div class="col-sm-10">
+                      <textarea class="form-control" rows="4" name="message"><?php echo htmlspecialchars($_POST['message']);?></textarea>
+                      <?php echo "<p class='text-danger'>$errMessage</p>";?>
+                  </div>
               </div>
-              <div class="item">
-                <img src="../images/carousel-main/montaje02.jpg" style="width: 100%" alt="Carousel 4" />
-                <div class="carousel-caption"></div>
+              <div class="form-group">
+                  <label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
+                  <div class="col-sm-10">
+                      <input type="text" class="form-control" id="human" name="human" placeholder="Tu respuesta">
+                      <?php echo "<p class='text-danger'>$errHuman</p>";?>
+                  </div>
               </div>
-              <div class="item">
-                <img src="../images/carousel-main/montaje03.jpg" style="width: 100%" alt="Carousel 5" />
-                <div class="carousel-caption"></div>
+              <div class="form-group">
+                  <div class="col-sm-10 col-sm-offset-2">
+                      <input id="submit" name="submit" type="submit" value="Enviar" class="btn btn-primary">
+                  </div>
               </div>
-            </div> 
-
-            <!-- Controls -->
-            <a class="left carousel-control" href="#carousel-main" role="button" data-slide="prev">
-              <span class="glyphicon glyphicon-chevron-left"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#carousel-main" role="button" data-slide="next">
-              <span class="glyphicon glyphicon-chevron-right"></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div><!-- /.carousel -->
+              <div class="form-group">
+                  <div class="col-sm-10 col-sm-offset-2">
+                      <?php echo $result; ?>    
+                  </div>
+              </div>
+          </form> 
+          </div><!-- /.formulario-->
+          <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+           <ul>
+             <li>Tel: +34....</li>
+             <li>Mail: ...</li>
+             <li>Dirección: ...</li>
+           </ul>
+          </div>
         </div>
-      </div> <!-- /.row del carousel -->
+      </div> <!-- /.container del form -->
 
 
 
